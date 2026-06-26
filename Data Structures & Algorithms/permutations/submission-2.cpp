a7c3329e48944686e4c515f028beb42ce9d9514e
David Suchan <davidsuchan4@gmail.com>
2026-06-27 00:34:31 +0100

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> temp;
+        set<int> st;
+        dfs(nums, ret, temp, st);
+        return ret;
+
+
+    }
+
+    void dfs(vector<int>& nums, vector<vector<int>>& ret, vector<int>& temp, set<int>& st){
+        if(nums.size() <= temp.size()){
+            ret.push_back(temp);
+        }
+
+        for(int i{}; i < nums.size(); ++i){
+            if(!st.count(nums[i])){
+                temp.push_back(nums[i]);
+                st.insert(nums[i]);
+                dfs(nums, ret, temp, st);
+                temp.pop_back();
+                st.erase(nums[i]);
+            }
+        }
+
+    }
+};
