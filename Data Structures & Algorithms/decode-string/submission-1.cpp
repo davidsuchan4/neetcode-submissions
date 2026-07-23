29ee38d7476a0803316796600259a2bcf1c5f28b
David Suchan <davidsuchan4@gmail.com>
2026-07-23 15:27:27 +0100

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        
+        stack<string> stringStack;
+        stack<int> times;
+        string k{};
+        string cur{};
+
+        for(int i{}; i < s.size(); ++i){
+            if(isdigit(s[i])){
+                k += s[i];
+            }
+            else if(s[i] == '['){
+                times.push(stoi(k));
+                stringStack.push(cur);
+                k = "";
+                cur = "";
+            }
+            else if(s[i] >= 'a' && s[i] <= 'z'){
+                cur += s[i];
+            }
+            else if(s[i] == ']'){
+                string temp = cur;
+                cur = stringStack.top();
+                stringStack.pop();
+                for(int i{}; i < times.top(); ++i){
+                    cur += temp;
+                }
+                times.pop();
+            }
+        }
+        return cur;
+    }
+};
